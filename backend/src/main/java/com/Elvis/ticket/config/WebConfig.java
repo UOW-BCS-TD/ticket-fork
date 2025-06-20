bb2b6d5f79b50b58ff141890ae15f2faa191a9a5
Elvificent <elvischu91041128@gmail.com>
2025-06-21 01:18:52 +0800

    host

--- backend/src/main/java/com/Elvis/ticket/config/WebConfig.java
+++ backend/src/main/java/com/Elvis/ticket/config/WebConfig.java
@@ -16,7 +16,8 @@ public class WebConfig implements WebMvcConfigurer {
                     "http://10.14.0.2:5173",
                     "http://192.168.56.1:5173",
                     "http://10.2.67.76:5173",
-                    "http://172.24.80.1:5173"
+                    "http://172.24.80.1:5173",
+                    "http://43.228.124.29"
                 )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
