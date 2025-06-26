8afc2d4b71aa9ecd46cc67ef1d3672ce903606d4
Chu Chun Ning Elvis <113424314+Elvificent@users.noreply.github.com>
2025-06-26 14:33:44 +0800

    enhanced

--- frontend/src/Pages/Chatbot/Chatbot.jsx
+++ frontend/src/Pages/Chatbot/Chatbot.jsx
@@ -690,6 +690,8 @@ const Chatbot = () => {
                 <div className="wave wave-1"></div>
                 <div className="wave wave-2"></div>
                 <div className="wave wave-3"></div>
+                <div className="wave wave-4"></div>
+                <div className="wave wave-5"></div>
               </div>
             )}
           </div>
